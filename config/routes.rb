4f7973b0efd0e72a13cb9ddb4fbd7bb1a3db56b7
yuki doi <aoakamassyou@yahoo.co.jp>
2017-11-21 14:51:03 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
   
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
   
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
+  
+  get '/signup', to: 'users#new'
 
   root 'static_pages#home'
 end
